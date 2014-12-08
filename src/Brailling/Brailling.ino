e27550762eb788b61d1a45bc69f76e1937bd4a3e
Juha Kivekas <juha.kivekas@wippies.fi>
2014-12-08 23:19:39 +0200

    a bit of explanation

--- src/Brailling/Brailling.ino
+++ src/Brailling/Brailling.ino
@@ -1,2 +1,5 @@
+//opening this file in the Arduino IDE will let you uplad the Brailling program
+//change these paths to whatever is the path to the files, relative paths don't seem to work
+//click on upload, and you're good to go
 #include "/home/juha/Documents/BraillePad/src/BraillePad.cpp"
 #include "/home/juha/Documents/BraillePad/src/CharMap.cpp"
